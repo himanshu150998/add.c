34223a234a6a7d130131378da90fc8b9e5c343ee
himanshu150998 <101660995+himanshu150998@users.noreply.github.com>
2022-03-21 13:59:57 +0530

    Add files via upload

---
+++ secondlargestarr.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+#include<climits>
+
+using namespace std;
+
+int main() 
+{
+    
+int i,n,a[100];
+    
+cout<<"enter the elements";
+    
+cin>>n;
+    
+for(i=0;i<n;i++)
+    
+{
+        
+cin>>a[i];
+        
+    
+}
+    
+int largest=INT_MIN;
+    
+int secondLargest=INT_MIN;
+   
+ for(i=0;i<n;i++)
+    
+{
+        
+if(a[i]>largest)
+        
+{
+            
+secondLargest=largest;
+            
+largest=a[i];
+        
+}
+        
+else
+        
+{
+            
+if(a[i]>secondLargest && a[i]<largest)
+           
+secondLargest=a[i];
+        
+}
+    
+}
+    
+printf("secondLargest is %d\n",secondLargest);
+
+    
+return 0;
+
+}
